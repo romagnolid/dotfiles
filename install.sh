53eb9407570ca59efb3e7276f585772bac0c1e4f
Dario Romagnoli <dario.romagnoli87@gmail.com>
2018-04-19 09:33:07 +0200

    Add inputrc: better tab completition

--- install.sh
+++ install.sh
@@ -10,3 +10,4 @@ ln -sf $PWD/.vimrc $HOME/.vimrc
 ln -sf $PWD/.tmux.conf $HOME/.tmux.conf
 ln -sf $PWD/.Rprofile $HOME/.Rprofile
 ln -sf $PWD/.gitconfig $HOME/.gitconfig
+ln -sf $PWD/.inputrc $HOME/.inputrc
