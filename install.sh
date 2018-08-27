0bcb9d7a629a92228d1d2568a534698c7e44d600
Dario Romagnoli <dario.romagnoli87@gmail.com>
2018-08-27 10:01:59 +0200

    install updates: don't create backup files

--- install.sh
+++ install.sh
@@ -6,16 +6,10 @@ if [[ $(basename $PWD) != "$folder" ]]; then
     exit 1
 fi
 
-cp $HOME/.vimrc      $HOME/.vimrc.bak
-cp $HOME/.tmux.conf  $HOME/.tmux.conf.bak
-cp $HOME/.Rprofile   $HOME/.Rprofile.bak
-cp $HOME/.gitconfig  $HOME/.gitconfig.bak
-cp $HOME/.inputrc    $HOME/.inputrc.bak
-cp $HOME/.bashrc     $HOME/.bashrc.bak
-
 ln -sf $PWD/.vimrc      $HOME/.vimrc
 ln -sf $PWD/.tmux.conf  $HOME/.tmux.conf
 ln -sf $PWD/.Rprofile   $HOME/.Rprofile
 ln -sf $PWD/.gitconfig  $HOME/.gitconfig
 ln -sf $PWD/.inputrc    $HOME/.inputrc
-ln -sf $PWD/.bashrc     $HOME/.bashrc
+
+
