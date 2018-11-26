97c15dd6024d76b77daf0d55247d5686bee00e00
Dario Romagnoli <dario.romagnoli87@gmail.com>
2018-11-26 16:46:41 +0100

    Add filetypes for vim csv plugin

--- install.sh
+++ install.sh
@@ -7,12 +7,13 @@ if [[ $(basename $PWD) != "$folder" ]]; then
 fi
 
 mkdir -p $HOME/.config/nvim
-ln -sf $PWD/init.vim          $HOME/.config/nvim
-ln -sf $PWD/bashrc            $HOME/.bashrc
-ln -sf $PWD/bash_aliases      $HOME/.bash_aliases
-ln -sf $PWD/Rprofile          $HOME/.Rprofile
 ln -sf $PWD/.gitconfig        $HOME/.gitconfig
+ln -sf $PWD/Rprofile          $HOME/.Rprofile
+ln -sf $PWD/bash_aliases      $HOME/.bash_aliases
+ln -sf $PWD/bashrc            $HOME/.bashrc
+ln -sf $PWD/filetype.vim      $HOME/.vim/
+ln -sf $PWD/init.vim          $HOME/.config/nvim
 ln -sf $PWD/inputrc           $HOME/.inputrc
+ln -sf $PWD/redshift.conf     $HOME/.config/
 ln -sf $PWD/tmux.conf         $HOME/.tmux.conf
 ln -sf $PWD/vimrc             $HOME/.vimrc
-ln -sf $PWD/redshift.conf     $HOME/.config/
