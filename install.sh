35153b93abeeff68133aec587a2ee373dbd437e6
Dario Romagnoli <dario.romagnoli87@gmail.com>
2018-12-21 15:05:43 +0100

    Physically add skeletons

--- install.sh
+++ install.sh
@@ -13,6 +13,7 @@ ln -sf $PWD/Rprofile          $HOME/.Rprofile
 ln -sf $PWD/bash_aliases      $HOME/.bash_aliases
 ln -sf $PWD/bashrc            $HOME/.bashrc
 ln -sf $PWD/filetype.vim      $HOME/.vim/
+ln -sf $PWD/skeleton*         $HOME/.vim/
 ln -sf $PWD/init.vim          $HOME/.config/nvim
 ln -sf $PWD/inputrc           $HOME/.inputrc
 ln -sf $PWD/redshift.conf     $HOME/.config/
