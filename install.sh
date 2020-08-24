71ca5d167368053d5a8a661d089031f2fed489ac
Dario Romagnoli <dario.romagnoli87@gmail.com>
2020-08-24 16:51:53 +0200

    zshrc

--- install.sh
+++ install.sh
@@ -10,16 +10,17 @@ fi
 mkdir -p $HOME/.config/nvim
 mkdir -p $HOME/.vim/after/colors/
 
-ln -sf $PWD/tender.vim        $HOME/.vim/after/colors/
-ln -sf $PWD/gitconfig         $HOME/.gitconfig
 ln -sf $PWD/Rprofile          $HOME/.Rprofile
 ln -sf $PWD/bash_aliases      $HOME/.bash_aliases
 ln -sf $PWD/bashrc            $HOME/.bashrc
 ln -sf $PWD/filetype.vim      $HOME/.vim/
-ln -sf $PWD/skeleton*         $HOME/.vim/
+ln -sf $PWD/gitconfig         $HOME/.gitconfig
 ln -sf $PWD/init.vim          $HOME/.config/nvim
 ln -sf $PWD/inputrc           $HOME/.inputrc
 ln -sf $PWD/redshift.conf     $HOME/.config/
+ln -sf $PWD/skeleton*         $HOME/.vim/
+ln -sf $PWD/tender.vim        $HOME/.vim/after/colors/
 ln -sf $PWD/tmux.conf         $HOME/.tmux.conf
 ln -sf $PWD/tmux.reset.conf   $HOME/.tmux/
 ln -sf $PWD/vimrc             $HOME/.vimrc
+ln -sf $PWD/zshrc             $HOME/.zshrc
