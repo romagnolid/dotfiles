fdbe7de920710938e05d919b26f8d17b5858945d
Dario Romagnoli <dario.romagnoli87@gmail.com>
2025-11-11 15:30:03 +0100

    update install.sh

--- install.sh
+++ install.sh
@@ -6,20 +6,21 @@ if [[ $(basename $PWD) != "$folder" ]]; then
     exit 1
 fi
 
-mkdir -p $HOME/.config/nvim
-mkdir -p $HOME/.vim/after/colors/
+# mkdir -p $HOME/.config/nvim
+# mkdir -p $HOME/.vim/after/colors/
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
+# ln -sf $PWD/filetype.vim      $HOME/.vim/
+# ln -sf $PWD/init.vim          $HOME/.config/nvim
+# ln -sf $PWD/skeleton*         $HOME/.vim/
+# ln -sf $PWD/tender.vim        $HOME/.vim/after/colors/
 # ln -sf $PWD/tmux.reset.conf   $HOME/.tmux/
+# ln -sf $PWD/vimrc             $HOME/.vimrc
 # ln -sf $PWD/zshrc             $HOME/.zshrc
+#ln -sf $PWD/inputrc           $HOME/.inputrc
+ln -sf $PWD/Rprofile          $HOME/.Rprofile
 ln -sf $PWD/bash_aliases      $HOME/.bash_aliases
 ln -sf $PWD/bashrc            $HOME/.bashrc
-ln -sf $PWD/filetype.vim      $HOME/.vim/
 ln -sf $PWD/gitconfig         $HOME/.gitconfig
-ln -sf $PWD/init.vim          $HOME/.config/nvim
-ln -sf $PWD/inputrc           $HOME/.inputrc
-ln -sf $PWD/Rprofile          $HOME/.Rprofile
-ln -sf $PWD/skeleton*         $HOME/.vim/
-ln -sf $PWD/tender.vim        $HOME/.vim/after/colors/
 ln -sf $PWD/tmux.conf         $HOME/.tmux.conf
-ln -sf $PWD/vimrc             $HOME/.vimrc
+cp -r nvim $HOME/.config
